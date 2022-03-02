0e4d59147500fbc6b34e7f94c1681e24ed5fe823
wellingtonpleni <wellingtonpleni@gmail.com>
2022-03-02 17:16:52 -0300

    Bootcamp e Devs

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -21,8 +23,36 @@ public class Main {
         mentoria.setDescricao("Aprendendo Java");
         mentoria.setData(LocalDate.now());
 
+        /*
         System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria);
+         */
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("GFT Start");
+        bootcamp.setDescricao("Seja um desenvolvedor Java");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev dev1 = new Dev();
+        dev1.setNome("Wellington");
+        dev1.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos: " + dev1.getConteudosInscritos());
+        dev1.progredir();
+        System.out.println("Conteúdos inscritos: " + dev1.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos: " + dev1.getConteudosConcluidos());
+        System.out.println("XP: " + dev1.calcularTotalXp());
+
+        Dev dev2 = new Dev();
+        dev2.setNome("Camila");
+        dev2.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos: " + dev2.getConteudosInscritos());
+        dev2.progredir();
+        System.out.println("Conteúdos inscritos: " + dev2.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos: " + dev2.getConteudosConcluidos());
+        System.out.println("XP: " + dev2.calcularTotalXp());
+
     }
 }
